109d10ff20e712c4b4816cc53c3429b6a616b503
klachuriye001 <klachuriye001@klachuriye00187.us.nam.ad.pwcinternal.com>
2019-09-08 09:10:09 -0400

    New Changes

--- HybridFrameWork/src/test/java/comm/learnautomation/testcases/LogInTestCrm.java
+++ HybridFrameWork/src/test/java/comm/learnautomation/testcases/LogInTestCrm.java
@@ -5,14 +5,15 @@ import org.testng.annotations.Test;
 
 import comm.automation.pages.BaseClass;
 import comm.automation.pages.CrmLogInPage;
+import comm.learnautomation.utility.ExcelDataProvider;
 
 public class LogInTestCrm extends BaseClass {
 
 	@Test
 	public void loginApp() {
-
+		ExcelDataProvider excel = new ExcelDataProvider();
 		CrmLogInPage logInPage = PageFactory.initElements(driver, CrmLogInPage.class);
-		logInPage.logInCrm("Abc", "Xyz");
+		logInPage.logInCrm(excel.getStringData("Log", 0, 0), excel.getStringData("Log", 0, 1));
 
 	}
 
--- HybridFrameWork/src/test/java/comm/learnautomation/utility/ExcelDataProvider.java
+++ HybridFrameWork/src/test/java/comm/learnautomation/utility/ExcelDataProvider.java
@@ -4,10 +4,35 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ExcelDataProvider {
-	public ExcelDataProvider() throws FileNotFoundException {
+	XSSFWorkbook wb;
+
+	public ExcelDataProvider() {
 		File src = new File("./TestData/Testdata.xlsx");
-		FileInputStream fi = new FileInputStream(src);
-		
+		try {
+			FileInputStream fi = new FileInputStream(src);
+			wb = new XSSFWorkbook(fi);
+		} catch (Exception e) {
+			System.out.println("Not able to read Excel " + e.getMessage());
+		}
+
+	}
+
+	public String getStringData(String sheetName, int rowNo, int colNo) {
+		return wb.getSheet(sheetName).getRow(rowNo).getCell(colNo).getStringCellValue();
+	}
+
+	public String getStringData(int sheetindex, int rowNo, int colNo) {
+		return wb.getSheetAt(sheetindex).getRow(rowNo).getCell(colNo).getStringCellValue();
+	}
+
+	public double getIntData(String sheetName, int rowNo, int colNo) {
+		return wb.getSheet(sheetName).getRow(rowNo).getCell(colNo).getNumericCellValue();
+	}
+
+	public double getIntData(int sheetindex, int rowNo, int colNo) {
+		return wb.getSheetAt(sheetindex).getRow(rowNo).getCell(colNo).getNumericCellValue();
 	}
 }
